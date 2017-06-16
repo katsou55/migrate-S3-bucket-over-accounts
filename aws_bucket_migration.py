59e9f2c71e2fb6954552298aa139c2d74971b3b1
Carlos Rodrigues <carlosfeup55@gmail.com>
2017-06-16 22:53:34 +0100

    script to migrate buckets between accounts v2

--- aws_bucket_migration.py
+++ aws_bucket_migration.py
@@ -1,30 +1,20 @@
 ### Migrate bucket between accounts
 
-## Create migration bucket
-#aws s3 mb s3://m_bucket
-
-##aws s3 cp s3://target_bucket/ s3://m_bucket/ --recursive 
-
-## Delete original bucket
-#aws s3 rb s3://target_bucket
-
-## Create original bucket (on the account using the script)
-#aws s3 mb s3://target_bucket
-
-## Move from migration bucket to new bucket
-#aws s3 cp s3://m_bucket/ s3://target_bucket/ --recursive 
-
-## Delete m_bucket content
-## Delete migration bucket
-#aws s3 rb s3://m_bucket
-
 #EXAMPLE: python aws_bucket_migration.py migration-carlos-999 carlos-test-999 marionete
+
+#STEPS
+#1) Create migration bucket
+#2) Migration
+#3) Delete original bucket
+#4) Create original bucket (on the account using the script)
+#5) Move from migration bucket to new bucket
+#6) Delete migration bucket
 
 import sys
 import subprocess
 
 #print(sys.argv[0],sys.argv[1],sys.argv[2],sys.argv[3])
-if(len(sys.argv) != 4):
+if(len(sys.argv) != 5):
     print("Incorrect number of args")
     sys.exit(1)
 elif(sys.argv[1] is sys.argv[2]):
@@ -33,7 +23,7 @@ elif(sys.argv[1] is sys.argv[2]):
 
 m_bucket = sys.argv[1]
 target_bucket = sys.argv[2]
-profile = sys.argv[3]
+profile_new_bucket = sys.argv[3]
 
 command = """
 aws s3 mb s3://{0} --profile {2} &&
@@ -41,9 +31,11 @@ aws s3 cp s3://{1}/ s3://{0}/ --recursive --profile {2} &&
 aws s3 rb s3://{1} --force --profile {2} &&
 aws s3 mb s3://{1} --profile {2} &&
 aws s3 cp s3://{0}/ s3://{1}/ --recursive --profile {2}
-""".format(m_bucket, target_bucket, profile)
+""".format(m_bucket, target_bucket, profile_new_bucket)
 
-rm_migration_bucket = "aws s3 rb s3://{0} --force --profile {2}".format(m_bucket, target_bucket, profile)
+rm_migration_bucket = """
+aws s3 rb s3://{0} --force --profile {2}
+""".format(m_bucket, target_bucket, profile_new_bucket)
 
 subprocess.check_output(command, shell=True)
 subprocess.check_output(rm_migration_bucket, shell=True)
